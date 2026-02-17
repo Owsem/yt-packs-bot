cc38b920eb1ed5e2ea240b29ae143f585ecb69cc
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 00:57:48 +0330

    Update pack.js

--- models/pack.js
+++ models/pack.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const packSchema = new mongoose.Schema({
+  pack_id: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String },
+  sample_url: { type: String },
+  file_url: { type: String }
+});
+
+module.exports = mongoose.model('Pack', packSchema);
 
