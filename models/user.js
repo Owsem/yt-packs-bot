92e802e6370c2a059ceceb3f257414a7068f8132
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 00:57:31 +0330

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  user_id: { type: Number, required: true, unique: true },
+  username: { type: String },
+  first_seen: { type: Date, default: Date.now },
+  last_seen: { type: Date, default: Date.now },
+  email: { type: String, default: null },
+  phone: { type: String, default: null }
+});
+
+module.exports = mongoose.model('User', userSchema);
 
