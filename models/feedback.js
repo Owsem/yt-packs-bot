04b2da95f91208c319e710fe38fb670692eb44ff
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 00:58:16 +0330

    Update feedback.js

--- models/feedback.js
+++ models/feedback.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+  user_id: { type: Number, required: true },
+  type: { type: String, enum: ['text', 'voice', 'photo'], required: true },
+  content: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
 
