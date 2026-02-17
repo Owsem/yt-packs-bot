6a2bd2bba9a42cbda76dba3259c64dfdcdc333cf
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 01:20:46 +0330

    Change feedback type to default to 'text'

--- models/feedback.js
+++ models/feedback.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
   user_id: { type: Number, required: true },
-  type: { type: String, enum: ['text', 'voice', 'photo'], required: true },
+  type: { type: String, enum: ['text'], default: 'text' },
   content: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Feedback', feedbackSchema);
-
