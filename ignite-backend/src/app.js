e3f940cfb5734561b1c08b23eaec7d44e00842f7
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 13:56:13 +0330

    Update app.js

--- ignite-backend/src/app.js
+++ ignite-backend/src/app.js
@@ -1,1 +1,24 @@
+import express from "express";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import themeRoutes from "./routes/themeRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/themes", themeRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Ignite API is running");
+});
+
+export default app;
 
