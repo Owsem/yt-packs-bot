5c239e5e57591bc8ea8fb98ff16bb09121319c01
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 13:58:34 +0330

    Add theme schema for mongoose model

--- ignite-backend/src/models/theme.js
+++ ignite-backend/src/models/theme.js
@@ -1,1 +1,30 @@
+import mongoose from "mongoose";
+
+const themeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    files: [
+      {
+        type: String, // URLs to your hosted assets
+      }
+    ],
+    category: {
+      type: String,
+      default: "general",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Theme", themeSchema);
 
