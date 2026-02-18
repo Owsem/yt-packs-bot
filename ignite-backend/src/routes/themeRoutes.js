401e2585175c834df1d5f8ab780fcabc42e6b4e9
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 13:59:42 +0330

    Update themeRoutes.js

--- ignite-backend/src/routes/themeRoutes.js
+++ ignite-backend/src/routes/themeRoutes.js
@@ -1,1 +1,22 @@
+import express from "express";
+import {
+  createTheme,
+  getThemes,
+  getThemeById,
+  deleteTheme,
+} from "../controllers/themeController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Public
+router.get("/", getThemes);
+router.get("/:id", getThemeById);
+
+// Admin only
+router.post("/", protect, admin, createTheme);
+router.delete("/:id", protect, admin, deleteTheme);
+
+export default router;
 
