74d3766e6e2022bc50eb2fcd832dea8f628e20d6
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 15:07:58 +0330

    Update Settings.js

--- ignite-backend/src/models/Settings.js
+++ ignite-backend/src/models/Settings.js
@@ -1,1 +1,15 @@
+import mongoose from "mongoose";
 
+const settingsSchema = new mongoose.Schema(
+  {
+    socialMedia: {
+      tiktok: { type: String, default: "" },
+      telegram: { type: String, default: "" },
+      pinterest: { type: String, default: "" },
+      reddit: { type: String, default: "" }
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Settings", settingsSchema);
