d1fb5c3a69fcb53d734932324896ea0440a9697f
Owsem <79610839+Owsem@users.noreply.github.com>
2026-02-18 14:02:04 +0330

    Update authMiddleware.js

--- ignite-backend/src/middleware/authMiddleware.js
+++ ignite-backend/src/middleware/authMiddleware.js
@@ -1,1 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
+// PROTECT ROUTES (requires login)
+export const protect = async (req, res, next) => {
+  let token;
+
+  // Check for token in headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Attach user to request (without password)
+      req.user = await User.findById(decoded.id).select("-password");
+
+      return next();
+    } catch
